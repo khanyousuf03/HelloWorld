a1a28b8fef2e9ff9c0515a2f8034d9b5fa3f0d0b
javaproject051218 <46148581+javaproject051218@users.noreply.github.com>
2018-12-26 10:46:52 +0530

    Add files via upload
    Added Activa.java file

---
+++ Activa.java
@@ -0,0 +1,16 @@
+class Activa extends Bike
+{
+Activa()
+{
+super(150,"Activa5G",200,15000);
+}	
+	 void gare()
+	 {
+	System.out.println("Activa bike haveing no gares");
+		}
+void breaking()
+{
+System.out.println("Activa bike haveing normal break");
+System.out.println("apply it carefully");
+}
+}
